99fba49889addd6357ffec4771e2b5cd1140b3fc
David Kalanda <davidmkalanda@gmail.com>
2017-10-30 02:17:28 +0000

    added winner pages

---
+++ app/controllers/winners_controller.rb
@@ -0,0 +1,8 @@
+class WinnersController < ApplicationController
+  def winners
+    @winners = Winner.all
+  end
+  def winner
+    @single = Winner.find(params[:id])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get '/about_us', to: 'pages#about', as: 'about'
 
   get 'lizard/:number', to: 'pages#lizards', as: 'lizards', number:/\d+/
+  
+  get 'winners', to: 'winners#winners', as: 'winners'
+
+  get 'winner/:id', to: 'winners#winner', as: 'winner', id:/\d+/
 
   root 'pages#home'
 
